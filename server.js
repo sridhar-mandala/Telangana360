42b2c250691ee81613c7fb2433d0b3084d75baed
sridhar-mandala <77686076+sridhar-mandala@users.noreply.github.com>
2021-03-09 14:24:10 -0600

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ http.createServer( function (request, response) {
       // Send the response body 
       response.end();
    });   
-}).listen(8081);
+}).listen(80);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:80/');
